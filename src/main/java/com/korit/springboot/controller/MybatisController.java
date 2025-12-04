9746d830fdf68a946fe07fdcec2d194a6f9819bc
code1218 <skjil1218@gmail.com>
2025-12-04 17:24:09 +0900

    mybatis

--- src/main/java/com/korit/springboot/controller/MybatisController.java
+++ src/main/java/com/korit/springboot/controller/MybatisController.java
@@ -1,6 +1,8 @@
 package com.korit.springboot.controller;
 
+import com.korit.springboot.dto.InsertProductReqDto;
 import com.korit.springboot.dto.InsertStudyReqDto;
+import com.korit.springboot.mapper.ProductMapper;
 import com.korit.springboot.mapper.StudyMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -16,10 +18,18 @@ public class MybatisController {
 
     @Autowired
     private StudyMapper studyMapper;
+    @Autowired
+    private ProductMapper productMapper;
 
     @PostMapping("/mybatis/study")
     public ResponseEntity<?> insert(@RequestBody InsertStudyReqDto dto) {
         studyMapper.insert(dto.getName(), dto.getAge());
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/mybatis/product")
+    public ResponseEntity<?> insertProduct(@RequestBody InsertProductReqDto dto) {
+        productMapper.insertProduct(dto.getSize(), dto.getProductName(), dto.getPrice());
         return ResponseEntity.ok().build();
     }
 
