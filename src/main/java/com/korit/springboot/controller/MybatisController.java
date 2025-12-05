50525cd4501f712f0c8e575b3dc9cb7cf07d7978
code1218 <skjil1218@gmail.com>
2025-12-05 11:49:55 +0900

    validation

--- src/main/java/com/korit/springboot/controller/MybatisController.java
+++ src/main/java/com/korit/springboot/controller/MybatisController.java
@@ -6,10 +6,12 @@ import com.korit.springboot.mapper.ProductMapper;
 import com.korit.springboot.mapper.StudyMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Map;
 
 
@@ -27,10 +29,22 @@ public class MybatisController {
         return ResponseEntity.ok().build();
     }
 
+    @GetMapping("/mybatis/study")
+    public ResponseEntity<List<Map<String, Object>>> getAll() {
+        return ResponseEntity.ok(studyMapper.findAll());
+    }
+
+    @GetMapping("/mybatis/study/names")
+    public ResponseEntity<List<String>> getNames() {
+        return ResponseEntity.ok(studyMapper.findAllName());
+    }
+
     @PostMapping("/mybatis/product")
     public ResponseEntity<?> insertProduct(@RequestBody InsertProductReqDto dto) {
         productMapper.insertProduct(dto.getSize(), dto.getProductName(), dto.getPrice());
         return ResponseEntity.ok().build();
     }
+
+
 
 }
